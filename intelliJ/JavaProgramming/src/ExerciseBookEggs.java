e8e2b0d85d7e0626cf99ac5a7df7683f7c3bc5d6
bensanta <BenSanta@protonmail.com>
2022-09-18 20:10:04 +0200

    Eggs Updated and values fixed

--- intelliJ/JavaProgramming/src/ExerciseBookEggs.java
+++ intelliJ/JavaProgramming/src/ExerciseBookEggs.java
@@ -1,26 +1,26 @@
 public class ExerciseBookEggs {
     public static void main(String[] args) {
         int numOfEggs = IBIO.inputInt("Enter the amount of eggs would you like to buy: "); //total eggs count
-        int numOfDozens = numOfEggs/12; //# of dozens
-        int numOfSingularEggs = numOfEggs%12; //extra apart from the dozen
+        int numOfDozens = numOfEggs / 12; //# of dozens
+        int numOfSingularEggs = numOfEggs % 12; //extra apart from the dozen
 
 
-        if(numOfDozens>-1 && numOfDozens<4){
+        if(numOfDozens >= 0 && numOfDozens < 4){
             double pricePerDozen = 0.50;
-            double pricePerSingularEggs = 0.083*pricePerDozen;
-            System.out.println("You will pay: $" + ((pricePerDozen*numOfDozens) + (numOfSingularEggs*pricePerSingularEggs)));
-        } else if(numOfDozens>3 && numOfDozens<6){
+            double pricePerSingularEggs = 0.083 * pricePerDozen * numOfSingularEggs;
+            System.out.println("You will pay: $" + ((pricePerDozen * numOfDozens) + pricePerSingularEggs));
+        } else if(numOfDozens > 3 && numOfDozens < 6){
             double pricePerDozen = 0.45;
-            double pricePerSingularEggs = 0.083*pricePerDozen;
-            System.out.println("You will pay: $" + ((pricePerDozen*numOfDozens) + (numOfSingularEggs*pricePerSingularEggs)));
-        } else if(numOfDozens>5 && numOfDozens<11){
-            double pricePerDozen = 0.45;
-            double pricePerSingularEggs = 0.083*pricePerDozen;
-            System.out.println("You will pay: $" + ((pricePerDozen*numOfDozens) + (numOfSingularEggs*pricePerSingularEggs)));
-        } else if(numOfDozens>10){
-            double pricePerDozen = 0.45;
-            double pricePerSingularEggs = 0.083*pricePerDozen;
-            System.out.println("You will pay: $" + ((pricePerDozen*numOfDozens) + (numOfSingularEggs*pricePerSingularEggs)));
+            double pricePerSingularEggs = 0.083 * pricePerDozen * numOfSingularEggs;
+            System.out.println("You will pay: $" + ((pricePerDozen * numOfDozens) + pricePerSingularEggs));
+        } else if(numOfDozens > 5 && numOfDozens < 11){
+            double pricePerDozen = 0.40;
+            double pricePerSingularEggs = 0.083 * pricePerDozen * numOfSingularEggs;
+            System.out.println("You will pay: $" + ((pricePerDozen * numOfDozens) + pricePerSingularEggs));
+        } else if(numOfDozens > 10){
+            double pricePerDozen = 0.35;
+            double pricePerSingularEggs = 0.083 * pricePerDozen * numOfSingularEggs;
+            System.out.println("You will pay: $" + ((pricePerDozen * numOfDozens) + pricePerSingularEggs));
         } else{
             System.out.println("Please enter a positive number!");
         }
